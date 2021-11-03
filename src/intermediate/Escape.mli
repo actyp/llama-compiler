eda5af859cfe522924f3db0363320a9674e4919b
actyp <el17115@mail.ntua.gr>
2021-11-03 17:50:32 +0200

    Keep only the necessary interface modifying accordingly Escape.ml

--- src/intermediate/Escape.mli
+++ src/intermediate/Escape.mli
@@ -3,14 +3,13 @@ module S = Symbol
 module T = Types
 module TA = TypedAst
 
-(** [tbl_entry] type holding infos about escaping vars and functions *)
-type tbl_entry =
-  | TempVarInfo of { dec_depth: int; esc_flag: bool; func_sym: S.symbol }
+(** [info] type holding infos about escaping vars and functions *)
+type info =
   | VarInfo of { dec_depth: int; func_sym: S.symbol; esc_offset: int }
   | FuncInfo of { dec_depth: int; esc_list: (S.symbol * T.ty) list }
 
-(** [info_tbl_t] type of hashtbl, mapping ints (obtained from symbols) to [tbl_entry] *)
-type info_tbl_t = (int, tbl_entry) H.t
+(** [info_tbl_t] type of hashtbl, mapping ints (obtained from symbols) to [info] *)
+type info_tbl_t = (int, info) H.t
 
 (** [analyse tast] traverses and analyses TypedAst returning the [info_tbl_t] hashtbl
     containing information about escaping variables and all functions *)
